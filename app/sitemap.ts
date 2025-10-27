5b6c2e56fbb916d9904cda7f6df51cfabc6bb50e
Gautham P Raj <gauthampraj2003@gmail.com>
2025-10-27 21:03:15 +0530

    edited sitemap config

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://saecusat.com'
+  const baseUrl = 'https://sae.cusat.co.in'
 
   return [
     {
